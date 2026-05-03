3236ba110d4fa4b6b251e07b3e31ea9676c553e2
tktnq2004 <tktnq2004@gmail.com>
2026-05-03 17:00:30 +0700

    hello world

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+// const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
