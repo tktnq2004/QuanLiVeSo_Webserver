48ff58dd02f89be01b1a811bd37be0eee7aa1622
bduytuan244 <bdtuan244@gmail.com>
2026-05-17 10:17:38 +0700

    Update capve

--- controllers/capve.controller.js
+++ controllers/capve.controller.js
@@ -9,4 +9,41 @@ const getAll = async (req, res) => {
     }
 };
 
-module.exports = { getAll };
+const getById = async (req, res) => {
+    try {
+        const data = await service.getById(req.params.id);
+        if (!data) return res.status(404).send('Không tìm thấy cặp vé');
+        res.json(data);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+const create = async (req, res) => {
+    try {
+        await service.create(req.body);
+        res.status(201).send('Created');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+const update = async (req, res) => {
+    try {
+        await service.update(req.params.id, req.body);
+        res.send('Updated');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+const remove = async (req, res) => {
+    try {
+        await service.remove(req.params.id);
+        res.send('Deleted');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+module.exports = { getAll, getById, create, update, remove };
--- services/capve.service.js
+++ services/capve.service.js
@@ -1,5 +1,9 @@
 const model = require('../models/capve.model');
 
 const getAll = () => model.getAll();
+const getById = (id) => model.getById(id);
+const create = (data) => model.create(data);
+const update = (id, data) => model.update(id, data);
+const remove = (id) => model.remove(id);
 
-module.exports = { getAll };
+module.exports = { getAll, getById, create, update, remove };
