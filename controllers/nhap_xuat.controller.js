0339f418bf68d8b0511a047345424359cc142d04
bduytuan244 <bdtuan244@gmail.com>
2026-05-17 09:54:30 +0700

    Update controller

--- controllers/nhap_xuat.controller.js
+++ controllers/nhap_xuat.controller.js
@@ -1,20 +1,20 @@
-const service = require('../services/nhap_xuat.service');
+//const service = require('../services/nhap_xuat.service');
 
-const create = async (req, res) => {
-    try {
-        await service.create(req.body);
-        res.send('Created');
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-};
+//const create = async (req, res) => {
+//    try {
+//        await service.create(req.body);
+//        res.send('Created');
+//    } catch (err) {
+//        res.status(400).json({ message: err.message });
+//    }
+//};
 
-const getAll = async (req, res) => {
-    const data = await service.getAll();
-    res.json(data);
-};
+//const getAll = async (req, res) => {
+//    const data = await service.getAll();
+//    res.json(data);
+//};
 
-module.exports = {
-    create,
-    getAll
-};
+//module.exports = {
+//    create,
+//    getAll
+//};
