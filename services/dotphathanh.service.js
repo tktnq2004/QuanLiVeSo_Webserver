0166569f039091d296dbd2460b4dcad9519e61de
TrungMinh <trungm0306@gmail.com>
2026-06-11 13:01:34 +0700

    save current work

--- services/dotphathanh.service.js
+++
@@ -1,9 +0,0 @@
-const model = require('../models/dotphathanh.model');
-
-const getAll = () => model.getAll();
-const getById = (id) => model.getById(id);
-const create = (data) => model.create(data);
-const update = (id, data) => model.update(id, data);
-const remove = (id) => model.remove(id);
-
-module.exports = { getAll, getById, create, update, remove };
--- services/hinhthucthanhtoan.service.js
+++
@@ -1,5 +0,0 @@
-const model = require('../models/hinhthucthanhtoan.model');
-
-const getAll = () => model.getAll();
-
-module.exports = { getAll };
